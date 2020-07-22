9478638b58096e1b5ce197289e4bd7ebabd5d1a2
Josivaldo Batista <josivaldo.fb@gmail.com>
2020-07-22 11:00:30 -0300

    Rest funcionando.(OK)

---
+++ src/main/java/br/com/jfb/MathController.java
@@ -0,0 +1,38 @@
+package br.com.jfb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+  @GetMapping("/sum/{numberOne}/{numberTwo}")
+  public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+      throws Exception {
+    if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+      throw new Exception();
+    }
+    Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+    return sum;
+  }
+
+  private Double convertToDouble(String strNumber) {
+    if (strNumber == null) {
+      return 0D;
+    }
+    String number = strNumber.replaceAll(",", ".");
+    if (isNumeric(number)) {
+      return Double.parseDouble(number);
+    }
+    return 0D;
+  }
+
+  private boolean isNumeric(String strNumber) {
+    if (strNumber == null) {
+      return false;
+    }
+    String number = strNumber.replaceAll(",", ".");
+    return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+  }
+}
